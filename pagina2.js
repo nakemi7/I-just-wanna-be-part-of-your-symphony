4a2ddfa0fd78d367f40dd722395fc5c9bc94246b
nakemi7 <141441780+nakemi7@users.noreply.github.com>
2024-11-01 08:31:42 -0300

    zz

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
     cartao.innerHTML = `
-        <div class="conteudo-cartao" onclick = "mostrarResposta(this)"> 
+        <div class="conteudo-cartao" onclick = "mostraResposta(this)"> 
             <h3>${categoria}</h3>
             <div class ="pergunta-cartao"> 
             <p>${pergunta}</p>
@@ -17,7 +17,7 @@ function criaCartao (categoria, pergunta, resposta){
     conteiner.appendChild(cartao);
 }
 
-function mostrarResposta(card){
+function mostraResposta(card){
     const resposta = card.querySelector('.resposta-cartao')
     if(resposta.style.display === 'none'){
        resposta.style.display = 'block';
